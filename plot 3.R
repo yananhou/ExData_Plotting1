59459907460663674eb166fa1263b51fb6fc6183
Yanan Hou <yn.hou.eng@gmail.com>
2017-07-25 17:10:35 -0500

    Delete plot 3.R

--- plot 3.R
+++
@@ -1,7 +0,0 @@
-# Plot 3
-
-plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
-lines(data$DateTime, data$Sub_metering_2, col = 'red')
-lines(data$DateTime, data$Sub_metering_3, col = 'blue')
-legend('topright', col = c('black', 'red', 'blue'), 
-       legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
